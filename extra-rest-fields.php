dd866384749162757d12c1bcfd1c6f71dd857b8d
Heikki Kurhinen <heikki.kurhinen@gmail.com>
2016-08-10 22:42:24 +0300

    Initial commit

---
+++ extra-rest-fields.php
@@ -0,0 +1,41 @@
+<?php
+defined( 'ABSPATH' ) or die( 'Access denied!' );
+
+/*
+Plugin name: WordPress Rest Extra Fields
+Plugin URI: http://example.com
+Description: Plugin which allows configuring rest-api to return some extra fields.
+Author: Metatavu Oy
+Author URI: https://metatavu.fi
+Version: 0.0.1
+*/
+
+if( !function_exists('register_extra_rest_fields') ) {
+
+  add_action( 'rest_api_init', 'register_extra_rest_fields' );
+  function register_extra_rest_fields() {
+    register_rest_field( 'post',
+      'location',
+      array(
+        'get_callback' => 'get_rest_meta_field',
+        'update_callback' => null,
+        'schema' => null,
+      )
+    );
+    register_rest_field( 'post',
+      'open',
+      array(
+        'get_callback' => 'get_rest_meta_field',
+        'update_callback' => null,
+        'schema' => null,
+      )
+    );
+  }
+  
+  function get_rest_meta_field( $object, $field_name, $request ) {
+    return get_post_meta( $object[ 'id' ], $field_name, true );
+  }
+
+}
+
+?>
