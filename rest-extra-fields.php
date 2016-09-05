a82995b00145e406540f9e96a0ece5ff3cb9454f
Heikki Kurhinen <heikki.kurhinen@gmail.com>
2016-09-05 19:55:26 +0300

    Added open field

--- rest-extra-fields.php
+++ rest-extra-fields.php
@@ -46,6 +46,14 @@ if( !function_exists('register_extra_rest_fields') ) {
         'schema' => null,
       )
     );
+    register_rest_field( 'post',
+      'open',
+      array(
+        'get_callback' => 'get_rest_meta_field',
+        'update_callback' => null,
+        'schema' => null,
+      )
+    );
   }
   
   function get_rest_meta_field( $object, $field_name, $request ) {
